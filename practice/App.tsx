bc8c4c21c5719070a29e783984fe823ad62c930e
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-08-29 23:25:42 +0900

    practic useMemo

--- practice/App.tsx
+++ practice/App.tsx
@@ -14,47 +14,44 @@ import {
   Text,
   useColorScheme,
   View,
-  Alert
+  Alert,
+  FlatList
 } from 'react-native';
 import * as D from './src/data'
 import { SafeAreaView, ScrollView } from 'react-native';
-import Person from './src/screens/Person';
 import { Colors } from 'react-native-paper';
 import Color from 'color';
 import { Platform, Dimensions } from 'react-native';
 import { ImageBackground, Image } from 'react-native';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import Person from './src/copy/Person';
+import { useClock } from './src/hooks'
+import Cache from './src/screens/Cache';
+import Memo from './src/screens/Memo';
+import Fibo from './src/screens/Fibo';
 
 const people = D.makeArray(100).map(D.createRandomPerson)
 const avatarUrl = D.randomAvatarUrl();
 const avatarSize = 50;
 
+const { width } = Dimensions.get('window');
+const numberOfComponents = 3
+
 const App = () => {
 
-  const children = people.map((person) => (
-    <Person key={person.id} person={person} />
-  ))
+  const time = useClock();
 
   const onIconPress = () => {
     Alert.alert('icon pressed');
   }
 
   return (
-        <SafeAreaView style={[styles.flex]}>
-          <ImageBackground 
-            style={[styles.flex]}
-            source={require('./src/assets/images/bg.jpg')}
-          >
-            <Image source={{uri: avatarUrl}} style={[styles.image]} />
-            <View style={[styles.flex, styles.padding10]}>
-              <Text style={[styles.regular]}>some text here</Text>
-              <Text style={[styles.medium]}>some text here2</Text>
-              <Text style={[styles.semiBold]}>some text here3</Text>
-              <Text style={[styles.bold]}>some text here4</Text>
-            </View>
-            <Icon name="home" size={50} color={Colors.lightBlue500} onPress={onIconPress}
-            />
-          </ImageBackground>
+        <SafeAreaView style={[styles2.safeAreaView]}>
+          <ScrollView horizontal contentContainerStyle={[styles2.contentContainer]}>
+            <Cache />
+            <Memo />
+            <Fibo />
+          </ScrollView>
         </SafeAreaView>
   );
 };
@@ -62,13 +59,19 @@ const App = () => {
 const styles = StyleSheet.create({
   text: {fontSize: 25, color: Color(Colors.blue500).alpha(0.7).lighten(0.9).string()},
   imageBackground: {padding: 10},
-  flex: {flex: 1},
+  flex: {flex: 1, backgroundColor: Colors.lightBlue100},
   padding10: {padding: 10},
   image: {width: avatarSize, height: avatarSize, borderRadius: avatarSize/2},
   regular: {fontFamily: 'DancingScript-Regular'},
   medium: {fontFamily: 'DancingScript-Medium'},
   semiBold: {fontFamily: 'DancingScript-SemiBold'},
-  bold: {fontFamily: 'DancingScript-Bold'}
+  bold: {fontFamily: 'DancingScript-Bold'},
+  itemSeparator: {borderWidth: 1, borderColor: Color(Colors.grey500).lighten(0.3).string()}
 });
+
+const styles2 = StyleSheet.create({
+  safeAreaView: {flex:1},
+  contentContainer: {width: width * numberOfComponents}
+})
 
 export default App;
