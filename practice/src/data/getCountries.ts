e909555362c4d96002f20b107809f70680895350
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-09-03 22:13:12 +0900

    practice fetch

--- practice/src/data/getCountries.ts
+++ practice/src/data/getCountries.ts
@@ -1,15 +1,16 @@
-import { ICountry } from "./ICountry";
+import {ICountry} from './ICountry';
 
-export const getCountries = ():Promise<ICountry[]> => new Promise((resolve, reject) => {
+export const getCountries = (): Promise<ICountry[]> =>
+  new Promise((resolve, reject) => {
     fetch('https://restcountries.eu/rest/v2/all')
-    .then((res) => res.json())
-    .then((result: any[]) => {
+      .then(res => res.json())
+      .then((result: any[]) => {
         const countries = result.map((data: any) => {
-            const {region, subregion, name, capital, population} = data;
-            return {region, subregion, name, capital, population} as ICountry
-        })
+          const {region, subregion, name, capital, population} = data;
+          return {region, subregion, name, capital, population} as ICountry;
+        });
 
         resolve(countries);
-    })
-    .catch(reject)
-})
+      })
+      .catch(reject);
+  });
--- practice/src/screens/Fetch.tsx
+++ practice/src/screens/Fetch.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, FLatList } from 'react-native';
+import { StyleSheet, View, Text, FlatList } from 'react-native';
 import {Colors} from 'react-native-paper';
 import * as D from '../data'
 import Country from './Country';
@@ -12,14 +12,27 @@ export default function Fetch() {
     const [countries, setCountries] = useState<D.ICountry[]>([])
     const [error, setError] = useState<Error | null>(null);
 
+    useEffect(() => {
+        D.getCountries().then(setCountries).catch(setError)
+    }, [])
+
     return (
         <View style={[styles.view]}>
-            <Text style={[styles.text]}>{title}</Text>
+            <Text style={[styles.title]}>{title}</Text>
+            {error && <Text>{error.message}</Text>} 
+            <FlatList 
+                data={countries}
+                showsVerticalScrollIndicator={false}
+                renderItem={({item}) => <Country country={item} />}
+                keyExtractor={(item, index) => index.toString()}
+                ItemSeparatorComponent={() => <View style={styles.separator} />}
+            />
         </View>
     )
 }
 
 const styles = StyleSheet.create({
-    view: {flex: 1, padding: 5, backgroundColor: Colors.blue900},
-    text: {fontSize: 20, color: 'white'}
+    view: {flex: 1, alignItems: 'center', backgroundColor: Colors.blue100},
+    title: {fontSize: 30, fontWeight: '600'},
+    separator: {borderBottomColor: Colors.blue50, borderBottomWidth: 1}
 })
