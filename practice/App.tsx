e9d1544288cf74ddfc552844ea0ad3cebbfca21e
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-09-16 23:28:47 +0900

    create theme component

--- practice/App.tsx
+++ practice/App.tsx
@@ -1,13 +1,40 @@
 import React, {useState, useCallback} from 'react';
+import 'react-native-gesture-handler';
 import MainNavigator from './src/screens/MainNavigator';
+import {enableScreens} from 'react-native-screens';
+import {
+  NavigationContainer,
+  DefaultTheme,
+  DarkTheme,
+} from '@react-navigation/native';
+import {SafeAreaProvider} from 'react-native-safe-area-context';
+import {AppearanceProvider, useColorScheme} from 'react-native-appearance';
+import {ToggleThemeProvider} from './src/contexts';
+
+enableScreens();
 
 const App = () => {
+  const scheme = useColorScheme();
+  const [theme, setTheme] = useState(
+    scheme === 'dark' ? DarkTheme : DefaultTheme,
+  );
+
+  const toggleTheme = useCallback(
+    () => setTheme(({dark}) => (dark ? DefaultTheme : DarkTheme)),
+    [],
+  );
 
   return (
-    <MainNavigator />
+    <AppearanceProvider>
+      <ToggleThemeProvider toggleTheme={toggleTheme}>
+        <SafeAreaProvider>
+          <NavigationContainer theme={theme}>
+            <MainNavigator />
+          </NavigationContainer>
+        </SafeAreaProvider>
+      </ToggleThemeProvider>
+    </AppearanceProvider>
   );
 };
-
-
 
 export default App;
--- practice/src/screens/MainNavigator.tsx
+++ practice/src/screens/MainNavigator.tsx
@@ -1,14 +1,38 @@
 import React from 'react'
-import { StyleSheet, View, Text } from 'react-native'
+import { StyleSheet, Switch, View, Text } from 'react-native'
+import { SafeAreaView } from 'react-native-safe-area-context'
+import { useTheme } from '@react-navigation/native'
+import { useToggleTheme } from '../contexts'
+
 
 export default function MainNavigator() {
-  return (
-    <View style={[styles.view]}>
 
-    </View>
+  const theme = useTheme()
+  const toggleTheme = useToggleTheme()
+
+
+  return (
+    <SafeAreaView style={[styles.flex]}>
+      <View style={[styles.view, {backgroundColor: theme.colors.background}]}>
+        <View style={[styles.topBar, {backgroundColor: theme.colors.primary}]}>
+          <Switch value={theme.dark} onValueChange={toggleTheme} />
+        </View>
+        <View style={[styles.view]}>
+          <Text style={[styles.text, {color: theme.colors.text}]}>
+            This is top text.
+          </Text>
+          <Text style={[styles.text, {color: theme.colors.text}]}>
+            This is bottom text.
+          </Text>
+        </View>
+      </View>
+    </SafeAreaView>
   )
 }
 
 const styles = StyleSheet.create({
-  view: { flex: 1, alignItems: 'center', justifyContent: 'space-between'}
+  flex: {flex: 1},
+  view: { flex: 1, alignItems: 'center', justifyContent: 'space-between'},
+  topBar: {width: '100%', flexDirection: 'row', padding: 5, justifyContent: 'flex-end'},
+  text: {fontSize: 20}
 })
