667e50f6b1c493780c931a687793eb4db8c16ea2
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-09-21 00:01:06 +0900

    useMemo, useCallback

--- practice/App.tsx
+++ practice/App.tsx
@@ -1,31 +1,59 @@
-import React from 'react';
-import { StyleSheet, SafeAreaView, ScrollView, Dimensions } from 'react-native';
-import Cache from './src/screens/Cache'
-import Memo from './src/screens/Memo'
-import Fibo from './src/screens/Fibo';
+import React, { useMemo, useState } from 'react';
+import type { FC } from 'react'
+import { StyleSheet, SafeAreaView, ScrollView, Dimensions, FlatList, Text, View } from 'react-native';
+import { Colors } from 'react-native-paper';
+import PersonUsingValueState from './src/screens/PersonUsingValueState';
+import PersonUsingObjectState from './src/screens/PersonUsingObjectState';
+import PersonUsingPassingState from './src/screens/PersonUsingPassingState';
+import * as D from './src/data'
+import TopBar from './src/screens/TopBar';
 
 const { width } = Dimensions.get('window')
-const numberOfComponents = 3
 
-const App = () => {
+type PersonInformation = {
+  title: string
+  Component: FC<any>
+}
+
+const personInformations: PersonInformation[] = [
+  {title: 'PersonUsingValueState', Component: PersonUsingValueState},
+  {title: 'PersonUsingObjectState', Component: PersonUsingObjectState},
+  {title: 'PersonUsingPassingState', Component: PersonUsingPassingState},
+]
+
+const numberOfComponents = personInformations.length
+
+export default function App() {
+
+  const [people, setPeople] = useState<D.IPerson[]>([])
+  const children = useMemo(() => personInformations.map(({title, Component}:PersonInformation) => (
+    <View style={{flex:1}} key={title}>
+      <Text style={[styles.text]}>{title}</Text>
+      <FlatList
+        data={people}
+        renderItem={({item}) => <Component person={item} />}
+        keyExtractor={(item, index) => item.id}
+        ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
+      />
+    </View>
+  )), [people.length])
   return (
-    <SafeAreaView style={[styles.safeAreaView]}>
+    <SafeAreaView style={[styles.flex]}>
+      <TopBar setPeople={setPeople}/>
       <ScrollView 
         horizontal
-        contentContainerStyle={[styles.contentContainerStyle]}
+        contentContainerStyle={[styles.horizontalScrollView]}
       >
-        <Cache />
-        <Memo />
-        <Fibo />
+        {children}
       </ScrollView>
     </SafeAreaView>
   );
 };
 
-export default App;
-
 
 const styles = StyleSheet.create({
-  safeAreaView: {flex: 1},
-  contentContainerStyle: { width: width * numberOfComponents}
+  flex: {flex:1},
+  itemSeparator: {borderWidth: 1, borderColor: Colors.grey500},
+  horizontalScrollView: {width: width * numberOfComponents},
+  text: {fontSize: 24, textAlign: 'center'}
 })
--- practice/src/screens/PersonUsingValueState.tsx
+++ practice/src/screens/PersonUsingValueState.tsx
@@ -1,4 +1,4 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useState } from 'react';
 import type {FC} from 'react';
 import {Text, View, Image, Alert} from 'react-native';
 import * as D from '../data';
@@ -8,17 +8,33 @@ import {Avatar, IconText} from '../components';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 import { Colors } from 'react-native-paper'
 
+
 moment.locale('ko');
 
 export type PersonProps = {
   person: D.IPerson;
 };
 
-const Person: FC<PersonProps> = ({person}) => {
+const PersonUsingValueState: FC<PersonProps> = ({person}) => {
   
   const avatarPressed = useCallback(() => Alert.alert('avatar pressed'), [])
-const deletePressed = useCallback(() => Alert.alert('deleted pressed'), []);
-const countIconPressed = useCallback((name: string) => () => Alert.alert(`${name} pressed`), [])
+  const deletePressed = useCallback(() => Alert.alert('deleted pressed'), []);
+  
+  const [comment, setComment] = useState<number>(person.counts.comment)
+  const [retweet, setRetweet] = useState<number>(person.counts.retweet)
+  const [heart, setHeart] = useState<number>(person.counts.heart);
+
+  const clickComment = useCallback(() => {
+    setComment(prev => prev + 1)
+  }, [])
+
+  const clickRetweet = useCallback(() => {
+    setRetweet(prev => prev + 1)
+  }, [])
+
+  const clickHeart = useCallback(() => {
+    setHeart(prev => prev + 1)
+  }, [])
 
   return (
     <View style={[styles.view]}>
@@ -47,13 +63,13 @@ const countIconPressed = useCallback((name: string) => () => Alert.alert(`${name
         <Text numberOfLines={3} ellipsizeMode="tail" style={[styles.text, styles.comments]}>{person.comments}</Text>
         <Image style={[styles.image]} source={{uri: person.image}}/>
         <View style={[styles.countsView]}>
-          <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('comment')} name="comment" size={24} color={Colors.blue500} textStyle={[styles.iconText]} text={person.counts.comment} />
-          <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('retweet')} name="twitter-retweet" size={24} color={Colors.purple500} textStyle={[styles.iconText]} text={person.counts.retweet} />
-          <IconText viewStyle={[styles.touchableIcon]} onPress={countIconPressed('heart')} name="heart" size={24} color={Colors.purple500} textStyle={[styles.iconText]} text={person.counts.heart} />
+          <IconText viewStyle={[styles.touchableIcon]} onPress={clickComment} name="comment" size={24} color={Colors.blue500} textStyle={[styles.iconText]} text={comment} />
+          <IconText viewStyle={[styles.touchableIcon]} onPress={clickRetweet} name="twitter-retweet" size={24} color={Colors.purple500} textStyle={[styles.iconText]} text={retweet} />
+          <IconText viewStyle={[styles.touchableIcon]} onPress={clickHeart} name="heart" size={24} color={Colors.purple500} textStyle={[styles.iconText]} text={heart} />
         </View>
       </View>
     </View>
   );
 };
 
-export default Person;
+export default PersonUsingValueState;
