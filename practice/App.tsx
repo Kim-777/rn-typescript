ecb76cf63e3599615d6f45f557cf78ce8da38ed1
Kim-777 <60419368+Kim-777@users.noreply.github.com>
2021-09-23 22:13:53 +0900

    practice hooks

--- practice/App.tsx
+++ practice/App.tsx
@@ -1,59 +1,17 @@
-import React, { useMemo, useState } from 'react';
-import type { FC } from 'react'
-import { StyleSheet, SafeAreaView, ScrollView, Dimensions, FlatList, Text, View } from 'react-native';
-import { Colors } from 'react-native-paper';
-import PersonUsingValueState from './src/screens/PersonUsingValueState';
-import PersonUsingObjectState from './src/screens/PersonUsingObjectState';
-import PersonUsingPassingState from './src/screens/PersonUsingPassingState';
-import * as D from './src/data'
-import TopBar from './src/screens/TopBar';
+import React from 'react';
+import { StyleSheet, SafeAreaView } from 'react-native';
+import MainNavigator from './src/screens/MainNavigator';
 
-const { width } = Dimensions.get('window')
-
-type PersonInformation = {
-  title: string
-  Component: FC<any>
-}
-
-const personInformations: PersonInformation[] = [
-  {title: 'PersonUsingValueState', Component: PersonUsingValueState},
-  {title: 'PersonUsingObjectState', Component: PersonUsingObjectState},
-  {title: 'PersonUsingPassingState', Component: PersonUsingPassingState},
-]
-
-const numberOfComponents = personInformations.length
 
 export default function App() {
-
-  const [people, setPeople] = useState<D.IPerson[]>([])
-  const children = useMemo(() => personInformations.map(({title, Component}:PersonInformation) => (
-    <View style={{flex:1}} key={title}>
-      <Text style={[styles.text]}>{title}</Text>
-      <FlatList
-        data={people}
-        renderItem={({item}) => <Component person={item} />}
-        keyExtractor={(item, index) => item.id}
-        ItemSeparatorComponent={() => <View style={styles.itemSeparator} />}
-      />
-    </View>
-  )), [people.length])
   return (
-    <SafeAreaView style={[styles.flex]}>
-      <TopBar setPeople={setPeople}/>
-      <ScrollView 
-        horizontal
-        contentContainerStyle={[styles.horizontalScrollView]}
-      >
-        {children}
-      </ScrollView>
+    <SafeAreaView style={[styles.safeAreaView]}>
+      <MainNavigator />
     </SafeAreaView>
   );
 };
 
 
 const styles = StyleSheet.create({
-  flex: {flex:1},
-  itemSeparator: {borderWidth: 1, borderColor: Colors.grey500},
-  horizontalScrollView: {width: width * numberOfComponents},
-  text: {fontSize: 24, textAlign: 'center'}
+  safeAreaView: {flex: 1},
 })
